aa359708555a3a5a357947555ba4b0123f40d01a
Sebastian Mischke <SebastianMischke@gmx.de>
2017-01-10 11:39:12 +0100

    Clear CDirectShow-class

--- WAV-MP3-Encoder/WAV-MP3-Encoder/DirectShow.h
+++ WAV-MP3-Encoder/WAV-MP3-Encoder/DirectShow.h
@@ -9,31 +9,10 @@ public:
 	~CDirectShow();
 
 	void Init();
-	void setWindow(OAHWND parentwindow);
-	void Stop();
-	void Resume();
-	void Pause();
-	void CleanUp();
-	void Vollbild(bool v);
-	void Run();
-
-	void setCurrentPosition(REFERENCE_TIME pos);
-	REFERENCE_TIME getCurrentPosition();
-	REFERENCE_TIME getLenght();
-
-	LONG GetIt(UINT wparam, LONG lparam);
-	void setNotifyWindow(UINT NEAR WM_GRAPHNOTIFY);
-	void setVideoWindow();
-	void setFilename(CString filename);
-
-	CString filename = L"Confused.avi";
 
 private:
-	OAHWND window;
-	IGraphBuilder *pGraph; // ein Zeiger auf das COM-Interface
-	IMediaControl *pMediaControl;
-	IMediaEventEx *pEvent;
-	IVideoWindow *pVidWin = NULL;
-	IMediaSeeking *pSeek = NULL;
+	IGraphBuilder *m_Graph1;
+	IMediaControl *m_Ctrl1;
+	IMediaEventEx *m_Event1;
 };
 
